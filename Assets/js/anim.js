718777f15e57a52fe143be4797d7d6d75ca3f7d4
itsamansharma <itsamansharma@gmail.com>
2020-12-07 18:11:01 +0530

    setting up some animation

--- Assets/js/anim.js
+++ Assets/js/anim.js
@@ -8,5 +8,6 @@ tl.to(".text",{y:"0%", duration:1, stagger:0.3})
 
 tl.to(".slider",{y:"-100%", duration:1, delay:0.6})
 tl.to(".intro",{y:"-100%", duration:1},"-=1");
-tl.formTo("nav", {opacity:0}, {opacity:1 , duration:1},);
-tl.formTo(".big-text", {opacity:0}, {opacity:1 , duration:1},'-=1');
+
+tl.fromTo("nav", {opacity:0}, {opacity:1 , duration:1});
+tl.fromTo(".big-text", {opacity:0}, {opacity:1 , duration:1},'-=1');
